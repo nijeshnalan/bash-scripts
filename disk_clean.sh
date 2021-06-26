b4a161508837cb671fedd44073abc2aa64d26a50
U-EIGI\nijesh.n <nijesh.n@iblwsonijesh.corp.endurance.com>
2021-06-26 14:17:59 +0530

    Bash script to clear the old backup files

---
+++ disk_clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+NUM=`df -h / | tail -1 | awk {'print $5'} | cut -d% -f1`
+
+if [ $NUM -gt 80 ]
+
+then
+
+  for i in `ls -lt /home/thefunstations.com/backup | tail -2`; do rm -rf /home/thefunstations.com/backup/$i ; done
+   echo "Current Disk Usage on $HOSTNAME is $NUM. Needs manual Intervention" | /bin/mail -s "Backups Removed" allstaff@happybeehost.com
+
+fi
+
