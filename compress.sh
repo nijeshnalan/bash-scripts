9aec1c70d594af66b991f2d20ba7721fff499fea
Nijesh Nalan <niju4all@gmail.com>
2021-08-01 18:59:33 +0530

    Updated log paths

--- compress.sh
+++ compress.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+export PATH=/usr/lib64/qt-3.3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/root/bin
+
 NUM=`df -h /vz | tail -1 | awk {'print $5'} | cut -d% -f1`
 
 if [ $NUM -gt 95 ]
-
 then
 
-  /root/bin/compress >/dev/null 2>&1
-  echo "Current Disk Usage on $HOSTNAME is $NUM. Needs manual Intervention" | /bin/mail -s "Backups Removed" info@grepitout.com
-  
+  /usr/sbin/vzlist -a | awk '{print $1}' | sed '/CTID/d' > /usr/local/hbh_linux/tmp/ctid.txt
+  echo "Compact started on $(date)" >> /var/log/compact.log
+  for i in `cat /usr/local/hbh_linux/tmp/ctid.txt`
+  do
+  /usr/sbin/prl_disk_tool compact --hdd /vz/private/$i/root.hdd
+  done
+  echo "Compact finished on $(date)" >> /var/log/compact.log
+
 fi
