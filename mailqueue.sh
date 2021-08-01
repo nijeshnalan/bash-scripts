df6e3dd172f5527366a7802b624d8b93ddcacffc
Nijesh Nalan <niju4all@gmail.com>
2021-08-01 18:45:13 +0530

    Removed unused lines

--- mailqueue.sh
+++ mailqueue.sh
@@ -1,7 +1,10 @@
 #/bin/bash
-#for i in `postqueue -p | grep MAILER-DAEMON | awk {'print $1'}` ; do postsuper -d $i ; done
+
 NUM=`find /var/spool/postfix/deferred -type f | wc -l`
+
 if [ $NUM -gt 25 ]
+
 then echo "Current Email Queue size is $NUM" | /bin/mail -s "Email Queue Critical on $HOSTNAME nijesh@grepitout.com
 /usr/sbin/postsuper -d ALL
+
 fi
