b1bef617313ccb3c3d2d65bb77b1cf2b83b78bec
Nijesh V N <niju4all@gmail.com>
2022-07-24 16:24:26 +0530

    Fixed the issue with exit status

--- filesystem-error/check_fs_error.sh
+++ filesystem-error/check_fs_error.sh
@@ -12,6 +12,9 @@ STATE_UNKNOWN=3
 
 exitcode=$STATE_UNKNOWN
 
+COUNTER_CRITICAL=0
+COUNTER_UNKNOWN=0
+
 if [[ $# -ne 1 || $1 == "-h" ]]; then
     echo "Usage: ${0##*/} <partition list in quotes with "," as delimiter 'partition1,partition2'>" >&2
     exit $STATE_UNKNOWN
@@ -26,24 +29,35 @@ for partition in $partition_lists; do
   re="$partition$"
   realPartition=`df -h  | grep $re | awk {'print $1'}`
 
-  status=$(tune2fs -l $realPartition | grep state | awk {'print $3'})
+  status=$(/usr/bin/sudo /usr/sbin/tune2fs -l $realPartition | grep state | awk {'print $3'})
   exitStatus=$?
 
   if [[ "$status" == "clean" ]]; then
       result="[OK] Partition $partition is $status \n$result"
-      exitcode=$STATE_OK
 
   elif [ $exitStatus -ne 0 ]; then
       result="[UNKNOWN] Failed to get filesystem state \n$result"
-      exitcode=$STATE_UNKNOWN
+      let COUNTER_UNKNOWN++
 
   else
       result="[CRITICAL] Partition $partition is $status \n$result"
-      exitcode=$STATE_CRITICAL
+      let COUNTER_CRITICAL++
 
   fi
 
 done
+
+
+if [[ $COUNTER_CRITICAL -ge $COUNTER_UNKNOWN && $COUNTER_CRITICAL -ne 0 ]]; then
+  exitcode=$STATE_CRITICAL
+
+elif [[ $COUNTER_UNKNOWN -gt $COUNTER_CRITICAL && $COUNTER_CRITICAL -ne 0 ]]; then
+  exitcode=$STATE_UNKNOWN
+
+else
+  exitcode=$STATE_OK
+
+fi
 
 IFS=$OFS
 
